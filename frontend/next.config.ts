272f8b2e31827bbcb785e777e9752437d74e0729
BUI THE MINH THUAN <thuanthe81@gmail.com>
2025-11-25 08:56:29 +0700

    image qualities

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    qualities: [60, 65, 75, 85, 95],
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
